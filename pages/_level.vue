314fbe71b05e32f3c330538a09370bdeed3d5b62
Byung Joo Kim <byungkim82@gmail.com>
2021-06-19 12:22:33 -0400

    updated logging system

--- pages/_level.vue
+++ pages/_level.vue
@@ -90,31 +90,7 @@ export default {
       return await this.$axios.get(`/panel/close/${gameLevel}/${userToken}`)
     },
     async sendTaskPlannerRecords(data) {
-      return await this.$axios.post('/panel/save/', data, {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Request-Method': 'POST',
-          'Access-Control-Request-Headers':
-            'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
-          Origin: 'https://https://erebuild-taskplanner-v3.vercel.app/',
-        },
-      })
-    },
-    getCookie(name) {
-      let cookieValue = null
-      if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';')
-        for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim()
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === name + '=') {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
-            break
-          }
-        }
-      }
-      return cookieValue
+      return await this.$axios.post('/panel/save/', data)
     },
     sendPanelLogs(event) {
       const userEmail = event.data.user_email
