282341d43b8848ab35f596af28ac6169986500bf
Byung Joo Kim <byungkim82@gmail.com>
2021-06-29 10:21:32 -0400

    added user performance to route logging

--- middleware/logging.js
+++ middleware/logging.js
@@ -2,6 +2,7 @@ export default function ({ store, route }) {
   store.commit('setInteractions', {
     type: 'route',
     path: route.path,
+    performance: store.getters.competency,
     timestamp: Date.now(),
   })
 }
